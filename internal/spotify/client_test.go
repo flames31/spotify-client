05df4fe3a8721770567a807a2ab8de24b26a3d35
flames31 <rrahulao2000@gmail.com>
2025-07-01 09:47:53 -0400

    refactored tests

--- internal/spotify/client_test.go
+++ internal/spotify/client_test.go
@@ -7,6 +7,55 @@ import (
 	"github.com/flames31/spotify-client/internal/config"
 )
 
+func TestConnectPass(t *testing.T) {
+	t.Run("succesfully connects and fetches token from spotify", func(t *testing.T) {
+		client := &mockClient{
+			res: []byte(`{"access_token":"<mock_token>"}`),
+		}
+
+		cfg := &mockConfig{}
+		cfg.mockMap = map[string]any{
+			"SPOTIFY_CLIENT_ID": "test",
+			"SPOTIFY_SECRET":    "test",
+		}
+		mockApp := &config.App{
+			Client: client,
+			Config: cfg,
+		}
+
+		err := Connect(mockApp)
+		if err != nil {
+			t.Errorf("unexpected error on connect :%v", err)
+		}
+
+		if cfg.mockMap["SPOTIFY_TOKEN"] != "<mock_token>" {
+			t.Errorf("incorrect access token : wanted %v got %v", "<mock_token>", cfg.mockMap["SPOTIFY_TOKEN"])
+		}
+	})
+
+	t.Run("connect correctly sends an error", func(t *testing.T) {
+		client := &mockClient{
+			res: []byte(`{"access_token":"<mock_token>"}`),
+			err: errors.New("req failed"),
+		}
+
+		cfg := &mockConfig{}
+		cfg.mockMap = map[string]any{
+			"SPOTIFY_CLIENT_ID": "test",
+			"SPOTIFY_SECRET":    "test",
+		}
+		mockApp := &config.App{
+			Client: client,
+			Config: cfg,
+		}
+
+		err := Connect(mockApp)
+		if err == nil {
+			t.Error("expected error on connect")
+		}
+	})
+}
+
 type mockClient struct {
 	res []byte
 	err error
@@ -32,50 +81,4 @@ func (m *mockConfig) Set(key string, val any) {
 func (m *mockConfig) Write() error {
 	m.wrote = true
 	return nil
-}
-func TestConnectPass(t *testing.T) {
-	client := &mockClient{
-		res: []byte(`{"access_token":"<mock_token>"}`),
-	}
-
-	cfg := &mockConfig{}
-	cfg.mockMap = map[string]any{
-		"SPOTIFY_CLIENT_ID": "test",
-		"SPOTIFY_SECRET":    "test",
-	}
-	mockApp := &config.App{
-		Client: client,
-		Config: cfg,
-	}
-
-	err := Connect(mockApp)
-	if err != nil {
-		t.Errorf("unexpected error on connect :%v", err)
-	}
-
-	if cfg.mockMap["SPOTIFY_TOKEN"] != "<mock_token>" {
-		t.Errorf("incorrect access token : wanted %v got %v", "<mock_token>", cfg.mockMap["SPOTIFY_TOKEN"])
-	}
-}
-
-func TestConnectFail(t *testing.T) {
-	client := &mockClient{
-		res: []byte(`{"access_token":"<mock_token>"}`),
-		err: errors.New("req failed"),
-	}
-
-	cfg := &mockConfig{}
-	cfg.mockMap = map[string]any{
-		"SPOTIFY_CLIENT_ID": "test",
-		"SPOTIFY_SECRET":    "test",
-	}
-	mockApp := &config.App{
-		Client: client,
-		Config: cfg,
-	}
-
-	err := Connect(mockApp)
-	if err == nil {
-		t.Error("expected error on connect")
-	}
 }
